616c986ec36200a8f20ca044d032edd64a24f430
Elizaveta Vlasova <jelizavetavlasova@gmail.com>
2024-04-11 20:21:09 +0200

    test

---
+++ lekce-1/script.js
@@ -0,0 +1,13 @@
+document.body.innerHTML = "<h1>Hello world<h1>"
+document.body.innerHTML += "<h2>Ahoj<h2>"
+
+// document.body.innerHTML += 10 + 10
+// document.body.innerHTML += 10 - 10
+// document.body.innerHTML += 10 * 10
+// document.body.innerHTML += 10 / 10
+document.body.innerHTML += 3 % 2 //zbytek po deleni neboli modulo
+
+document.body.innerHTML += "<p>Jmenuji se " + "Liza</p>"
+document.body.innerHTML += "<p>Zaokrouhlene cislo " + Math.round(4.2) + "</p>"
+document.body.innerHTML += "<p> " + Math.random() + "</p>"
+document.body.innerHTML += "<p> " + Math.round(Math.random() * 10) + "</p>"
---
+++ lekce-2/script.js
@@ -0,0 +1,17 @@
+var name1 = "Tomas" //deprecated due to security reasons
+let name2 = "Honza" 
+const name3 = "Ondra"
+
+name2 = "Michal"
+//name3 = "Adela" konstantni promenna se nemeni
+
+document.body.innerHTML = name1 + " " + name2 + " " + name3
+
+let hourRate = 200
+let hoursAmount = 100
+let payroll = hourRate * hoursAmount
+
+hourRate = 400 //payroll will not change in this case
+
+document.body.innerHTML = payroll
+
